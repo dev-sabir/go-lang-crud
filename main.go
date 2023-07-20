42a8f92aab566304a0221ffa5e4e2d8856f5800c
Mohd Sabir <sabir9202@gmail.com>
2023-07-20 12:34:41 +0530

    added create, update and delete

--- main.go
+++ main.go
@@ -48,6 +48,88 @@ func getTaskByID(c *gin.Context) {
 	c.JSON(200, foundTask)
 }
 
+func createTask(c *gin.Context) {
+	var newTask Task
+
+	// Bind the request body to the newTask variable
+	if err := c.ShouldBindJSON(&newTask); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	// Assign a new unique ID for the task
+	newTask.ID = len(tasks) + 1
+
+	// Append the new task to the tasks slice
+	tasks = append(tasks, newTask)
+
+	c.JSON(201, newTask)
+}
+
+func updateTask(c *gin.Context) {
+	idParam := c.Param("id")
+
+	// Convert the idParam to an integer
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	// Find the task with the given ID
+	var foundTask *Task
+	for i, task := range tasks {
+		if task.ID == id {
+			foundTask = &tasks[i]
+			break
+		}
+	}
+
+	if foundTask == nil {
+		c.JSON(404, gin.H{"error": "Task not found"})
+		return
+	}
+
+	// Bind the request body to the foundTask variable for update
+	if err := c.ShouldBindJSON(foundTask); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	c.JSON(200, foundTask)
+}
+
+func deleteTask(c *gin.Context) {
+	idParam := c.Param("id")
+
+	// Convert the idParam to an integer
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	// Find the task with the given ID
+	var foundTaskIndex = -1
+	for i, task := range tasks {
+		if task.ID == id {
+			foundTaskIndex = i
+			break
+		}
+	}
+
+	if foundTaskIndex == -1 {
+		c.JSON(404, gin.H{"error": "Task not found"})
+		return
+	}
+
+	// Remove the task from the slice
+	tasks = append(tasks[:foundTaskIndex], tasks[foundTaskIndex+1:]...)
+
+	c.JSON(200, gin.H{"message": "Task deleted successfully"})
+}
+
+
 
 
 func main() {
@@ -55,6 +137,15 @@ func main() {
 
 	// Define the "GET" route to fetch data by ID
 	router.GET("/tasks/:id", getTaskByID)
+
+	// Define the "POST" route to create a new task
+	router.POST("/tasks", createTask)
+
+	// Define the "PUT" route to update an existing task
+	router.PUT("/tasks/:id", updateTask)
+
+	// Define the "DELETE" route to remove a task
+	router.DELETE("/tasks/:id", deleteTask)
 
 	router.Run(":8080")
 }
